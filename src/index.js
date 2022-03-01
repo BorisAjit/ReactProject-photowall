5338fc3e1371b9db8b98bb4b79035115263eca0d
Boris Ajit <borisajit33@gmail.com>
2022-03-02 00:01:26 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from './Components/Main';
-
+import './styles/stylesheet.css'
 // let todo = ['Go get a good job','Now get a life','Travel more and enjoy life'];
 
 // const element = React.createElement('ol',null, todo.map((task,index)=>React.createElement('li',{key:index},task)));
-
 
 ReactDOM.render(<Main/>, document.getElementById('root'));
