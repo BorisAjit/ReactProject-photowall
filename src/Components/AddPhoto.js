a701026209836cc48125d72b803b704964f580da
Boris Ajit <borisajit33@gmail.com>
2022-03-02 18:45:22 +0530

    implemented add function

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,13 +1,33 @@
 import React, {Component} from "react";
 
 class AddPhoto extends Component{
+
+    constructor(){
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        const imgLink = event.target.elements.link.value;
+        const imgDescription = event.target.elements.description.value;
+        const post = {
+            id: "0",
+            description: imgDescription,
+            imageLink: imgLink
+        }
+        if(imgDescription && imgLink){
+           this.props.onAddPhoto(post)
+       }
+    }
+
     render(){
         return <div>
             <h1>PhotoWall</h1>
             <div>
-                <form className="form">
-                    <input type="text" placeholder="link"></input>
-                    <input type="text" placeholder="description"></input>
+                <form className="form" onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="link" name="link"></input>
+                    <input type="text" placeholder="description" name="description"></input>
                     <button>Post</button>
                 </form>
             </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -9,7 +9,20 @@ class Main extends Component{
       super();
       console.log("Constructor method");
       this.state = {
-        posts : [],
+        posts : [{
+          id: "0",
+          description: "beautiful landscape",
+          imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+          "3919321_1443393332_n.jpg"
+          }, {
+          id: "1",
+          description: "Aliens???",
+          imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
+          }, {
+          id: "2",
+          description: "On a vacation!",
+          imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+          }]
       }
       this.removePhoto = this.removePhoto.bind(this);
     }
@@ -19,21 +32,12 @@ class Main extends Component{
         posts: state.posts.filter(post => post !== postRemoved)
       }))
     }
-    fetchPhotos(){
-      return [{
-        id: "0",
-        description: "beautiful landscape",
-        imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-        "3919321_1443393332_n.jpg"
-        }, {
-        id: "1",
-        description: "Aliens???",
-        imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
-        }, {
-        id: "2",
-        description: "On a vacation!",
-        imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-        }]
+
+    addPhoto(postSubmited) {
+      this.setState(state => ({
+        posts: this.state.posts.concat(postSubmited)
+      }));
+      console.log(this.state);
     }
 
     render(){
@@ -45,15 +49,15 @@ class Main extends Component{
               <PhotoWall posts = {this.state.posts} onRemovePhoto={this.removePhoto} onNavigate = {this.navigate}/>
             </div>)}
           />
-          <Route path="/AddPhoto" component={AddPhoto}/> 
+          <Route path="/AddPhoto" render={({history})=>(
+            <AddPhoto onAddPhoto = {(addedPost) => {
+              this.addPhoto(addedPost);
+              console.log(addedPost);
+              history.push('/')
+            }}/>
+          )}/> 
         </div>
       )
-    }
-
-    componentDidMount(){
-      console.log("component did mount method");
-      const data = this.fetchPhotos();
-      this.setState({posts : data});
     }
   }
 
